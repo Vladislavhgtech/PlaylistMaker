c0222f9e88015a670e68213db87a795d26b2940e
Vladislav <vld.85@inbox.ru>
2024-11-22 16:34:13 +0300

    Версия 1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,19 +1,49 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<MaterialButton>(R.id.searchButton)
+        val mediaButton = findViewById<MaterialButton>(R.id.mediaButton)
+        val settingsButton = findViewById<MaterialButton>(R.id.settingsButton)
+
+
+        // Устанавливаем обработчик на кнопку searchButton
+        searchButton.setOnClickListener {
+            // Переход на SearchActivity
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        // Устанавливаем обработчик на кнопку mediaButton
+        mediaButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        settingsButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
