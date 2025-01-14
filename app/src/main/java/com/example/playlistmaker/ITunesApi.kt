b3a3152f734f9d1096c412b3173605b2d539e269
Vladislav <vld.85@inbox.ru>
2025-01-14 18:38:57 +0300

    Спринт 11, версия 2.1

--- app/src/main/java/com/example/playlistmaker/ITunesApi.kt
+++ app/src/main/java/com/example/playlistmaker/ITunesApi.kt
@@ -9,6 +9,5 @@ interface ITunesApi {
     fun searchTracks(
         @Query("term") query: String, // Параметр запроса
         @Query("media") media: String = "music", // Тип медиа
-        @Query("limit") limit: Int = 10 // Ограничение на количество результатов
     ): Call<TrackResponse> // Возвращаем объект, который будет содержать ответ от API
 }
